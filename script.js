4dc130ed6512acc3f443af821f2f691478766159
abdulmenandineka <abdulmenandineka6@gmail.com>
2025-02-26 11:11:56 +0300

    adding some js interactions
    removing the lists,

--- script.js
+++ script.js
@@ -12,7 +12,45 @@ button.addEventListener('click', ()=>{
   newButtun.classList.add("new");
   ol.appendChild(newList);
   newList.append(newButtun);
+  let select = document.querySelector('select'),
+      all = document.querySelector("#all"),
+      done = select.children[1],
+      notDone = document.querySelector("#not-done"),
+      allList = document.querySelectorAll('li');
+      
+     
+  ipt.value = "";
+  newList.addEventListener("click", () => {
+    newList.classList.toggle("done");
+  });
+
+  newButtun.addEventListener("click", () => {
+    newList.remove();
+  });
+
+  select.addEventListener('change',()=>{
+      if(select.value =='all'){
+          newList.classList.add('display')
+          }else if(select.value =='done'){
+            if(newList.className !== 'done'){
+              newList.style.display = 'none'
+            }
+          }else if(select.value =='not done'){
+            if(newList.className === 'done'){
+              newList.style.display = 'none'
+            }
+          }
+      });
+      // console.log(select.id);
+      
   
+
+  // select.addEventListener('change',()=>{
+  //     if(newList.className!=='done'){
+  //         newList.style.display = 'none'
+  //     }
+  // })
+  // console.log( select);
 })
        
       
